a2a022f9b883666efaeb6155d24b23c0095cdd33
N0B0DY0013 <burtachame@gmail.com>
2024-08-26 22:17:56 +0800

    Day 17 to 18 code files

--- DAY 18/main.py
+++ DAY 18/main.py
@@ -1,20 +1,29 @@
-# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
 from turtle import Turtle, Screen
 import random
+import colorgram
 
 ninja = Turtle()
 screen = Screen()
+
+# sets the turtle on lower left
+screen.setworldcoordinates(-1, -1, screen.window_width() - 1, screen.window_height() - 1)
+
+# maximize window
+screenTk = screen.getcanvas().winfo_toplevel()
+screenTk.attributes("-zoom", 1)
+
 screen.colormode(255)
 
 ninja.shape("turtle")
-ninja.color("chartreuse2")
+ninja.color("black")
 ninja.speed(10)
+
 
 # draw square
 # for _ in range(4):
 #     ninja.forward(100)
 #     ninja.right(90)
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 # draw dash-line      
@@ -25,6 +34,7 @@ ninja.speed(10)
 #         ninja.penup()
         
 #     ninja.forward(10)
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 # draw triangle, square, pentagon, hexagon, heptagon, octagon, nonagon, decagon
@@ -38,6 +48,7 @@ ninja.speed(10)
 #     for l in range(s):
 #         ninja.forward(100)
 #         ninja.right(angle)
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 # draw random walk
@@ -59,16 +70,48 @@ ninja.speed(10)
 #         ninja.forward(30)
 #     else:
 #         ninja.backward(30)
-    
-
-# draw a spirograph
-for i in range(361):
-    ninja.setheading(i)
-    ninja.tiltangle(i+1*20)
-    ninja.circle(50)
-    #ninja.forward(i)
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------    
 
 
+# # draw a spirograph
+# total_circle = 5
+# circle_diameter = 50
+# angle = 360 / total_circle
+# for i in range(total_circle):
+#     ninja.pencolor(random.randint(0,255), random.randint(0,255), random.randint(0,255))
+#     ninja.circle(circle_diameter)
+#     ninja.right(angle)
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+
+color_list = []
+# getting colors from 'colorgram'
+# extracted_color = colorgram.extract('spot_painting.jpg', 30)
+
+# for color in extracted_color:
+#     color_list.append((color.rgb.r, color.rgb.g, color.rgb.b))
+
+# print(color_list)
+
+color_list = [(218, 165, 82), (82, 104, 151), (109, 154, 200), (243, 237, 225), (69, 127, 97), (129, 23, 61), (110, 173, 133), (158, 47, 81), (215, 202, 135), (152, 160, 53), (202, 116, 162), (239, 246, 243), (191, 69, 42), (36, 38, 81), (122, 116, 165), (205, 100, 54), (197, 83, 101), (87, 161, 117), (37, 49, 94), (73, 139, 180), (85, 24, 45), (220, 176, 197), (178, 187, 213), (173, 203, 186), (220, 179, 170), (81, 82, 27), (118, 34, 30), (92, 23, 17)]
+
+total_row = 10
+total_col = 10
+
+per_row = (list(screen.screensize())[1] / total_row)
+per_col =  (list(screen.screensize())[0] / total_col)
+
+ninja.penup()
+ninja.setpos(per_row, per_col)
+
+for i in range(total_row):
+    # ninja.setx(per_col)
+    ninja.sety(per_row * i)
+    for j in range(total_col):
+        ninja.setx(per_col * j)
+        ninja.dot(20, random.choice(color_list))
+
+ninja.setx(ninja.xcor() + (per_row))
+ninja.left(90)
 
 screen.exitonclick()
